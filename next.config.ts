14e7c3956b383fc07fc2c30e2a2d7666e3697995
Debashis Mitra <mdebashis268@gmail.com>
2024-12-21 05:23:55 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    dirs: ['pages', 'components', 'lib'],  // Define directories for linting
-    ignoreDuringBuilds: true,  // Ignore ESLint warnings during builds (optional)
-  },
-  reactStrictMode: true,  // Optional: Enable React Strict Mode
-};
+  };
 
 export default nextConfig;
